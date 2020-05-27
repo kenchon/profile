561e7f0b88b2b446bf89eed80cf451a3d3779d27
kenmikanmi <kenh5526@gmail.com>
2020-05-28 00:20:21 +0900

    Update profile

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,30 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Kenya Hondoh - 本藤 拳也</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>
+                    <h3>Who are you?</h3>
+                    こんにちは。首都圏でエンジニアをしている若者です。<br />
+                    業務の傍ら，クラウド・DevOps・組織文化などの怪しいことを学習している。<br /><br />
+
+                    {/* <a href="https://www.16personalities.com/ja/%E6%80%A7%E6%A0%BC%E8%A8%BA%E6%96%AD%E3%83%86%E3%82%B9%E3%83%88">16 Pesonality（性格診断）</a> の結果は <b>ENTJ</b>。<br />
+                    起業家気質・「感情よりも理屈」だそうですが，1年働いて「人は気持ちで動くから気持ちも大事だよなぁ」になっています 笑。<br /><br /> */}
+
+                    大学院では<a href="https://www.ipsj-kyushu.jp/page/ronbun/hinokuni/1008/upload/2018-1-0032_201902141506.pdf">画像認識の研究</a>をしていました。<br />
+                    どれも手広く浅いですが，趣味・興味関心は多岐にわたります 笑<br /><br />
+                    <h3>趣味・興味関心</h3>
+                    <ul>
+                        <li>何かつくること</li>
+                        <li>美術観賞，ドローイング</li>
+                        <li>ジャズを聴くこと，演奏</li>
+                        <li>FPS。CoD，Splatoon などのシューティングゲーム</li>
+                        <li>DX。ソフトウェアを活用したビジネス，組織論</li>
+                    </ul>
+                </p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                    <li><a href="#one" className="button next scrolly">こまかいプロフィールをみる</a></li>
                 </ul>
             </div>
         </div>
